67274605349774e0200c729f70042dc8509e9fe1
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-08-02 18:12:33 +0900

    [류지수] - spring 예제 프로젝트 최초 파일 추가

---
+++ src/main/java/com/nunbub/spring/ex/lifecycle/Ex01Controller.java
@@ -0,0 +1,38 @@
+package com.nunbub.spring.ex.lifecycle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/lifecycle/ex01")
+public class Ex01Controller {
+
+	// 문자열 response 만들기
+	@RequestMapping("/1")
+	@ResponseBody
+	public String stringResponse() {
+		return "예제 1번 문자열 담는 response";
+	}
+	
+	// map json response
+	// 과일이름 : 가격
+	@RequestMapping("/2")
+	@ResponseBody
+	public Map<String, Integer> mapResponse(){
+		
+		Map<String, Integer> map = new HashMap<>();
+		map.put("apple", 1500);
+		map.put("banana", 3000);
+		map.put("orange", 1000);
+		
+		return map;
+	}
+	
+	
+	
+	
+}
