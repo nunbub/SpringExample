272113f6b03a22e61f383707afa3fbc26f5c89d1
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-08-04 18:02:08 +0900

    [류지수] - used_goods 조회 예제

--- src/main/java/com/nunbub/spring/ex/database/UsedGoodsController.java
+++ src/main/java/com/nunbub/spring/ex/database/UsedGoodsController.java
@@ -1,17 +1,28 @@
 package com.nunbub.spring.ex.database;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.nunbub.spring.ex.database.bo.UsedGoodsBO;
+import com.nunbub.spring.ex.database.model.UsedGoods;
 
 // request를 전달 받고, response를 만드는 일
 @Controller
 public class UsedGoodsController {
+	
+	@Autowired
+	private UsedGoodsBO usedGoodsBO;
 
 	@RequestMapping("/database/ex01")
-	public ex01() {
+	@ResponseBody
+	public List<UsedGoods> ex01() {
 		
-		
-		
+		List<UsedGoods> usedGoodsList = usedGoodsBO.getUsedGoodsList();
+		return usedGoodsList;
 	}
 	
 }
--- src/main/java/com/nunbub/spring/ex/database/bo/UsedGoodsBO.java
+++ src/main/java/com/nunbub/spring/ex/database/bo/UsedGoodsBO.java
@@ -1,14 +1,26 @@
 package com.nunbub.spring.ex.database.bo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.nunbub.spring.ex.database.dao.UsedGoodsDAO;
+import com.nunbub.spring.ex.database.model.UsedGoods;
 
 // 전달 받은 데이터를 조작하거나 처리하고, 리턴 받은 데이터를 조작 처리하는 과정 ( = 로직 )
 @Service
 public class UsedGoodsBO {
+	
+	// 스프링이 객체를 생성해줌
+	@Autowired	
+	private UsedGoodsDAO usedGoodsDAO;
 
 	// usedGoods 테이블의 모든 행 정보 가져오는 메소드
-	public getUsedGoodsList() {
+	public List<UsedGoods> getUsedGoodsList() {
 		
+		List<UsedGoods> usedGoodsList = usedGoodsDAO.selectUsedGoodsList();
+		return usedGoodsList;
 	}
 	
 }
